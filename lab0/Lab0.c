b0ff793e2d496d1964482289c00b935420dfbaf4
Oscar Lee <owlee@umass.edu>
2015-09-19 15:39:36 -0400

    finish encryption but before test

--- lab0/Lab0.c
+++ lab0/Lab0.c
@@ -24,13 +24,7 @@ int main(int argc, char **argv) {
     char S[256];
     char T[256];
     char key[256];
-    
     int kLength; //key length
-
-    // // char inputChar;
-    // // char outputChar;
-    // // char prevChar;
-    // // int i;
 
     keyFile = fopen("./keyFile", "r");
     textFile = fopen("./keyFile", "r");
@@ -40,11 +34,11 @@ int main(int argc, char **argv) {
     assert(textFile != NULL)
     assert(encryptedFile != NULL)
 
+    // store keys into the key array
     kLength=0;
     while (kLength<256 && (inputChar = fgetc(keyF))!=EOF) {
       key[kLength] = inputChar;
       kLength++;
-    //    printf("hi %i", kLength);
     }
 
     Klength = 256;
@@ -55,40 +49,46 @@ int main(int argc, char **argv) {
 
     //swap 2. initial permutation
     char temp;
+    int j;
 
     for(int i=0; i<kLength; i++) {
         j = (j+S[i]+T[i]) % kLength;
         // temp = S[i];
         // S[i] = T[i];
         // T[i] = temp;
-        swap(&S[i],&T[i]);
+        swap(&S[i],&S[j]);
     }
 
-    // do the generation and XOR
+    char inputChar;
+    char outputChar;
 
+    // do the generation and XOR
+    while ((inputChar = fgetc(textFile))!=EOF) {
+        outputChar = inputChar ^ prevChar;
+        fputc(outputChar, encryptedFile);
+        // prevChar = outputChar;
+    }
+
+     //clean up
     fclose(keyFile);
     fclose(textFile);
     fclose(encryptedFile);
 
-    printf("Hello World");
+    printf("I have reached the end");
     return 0;
 }
 
 char generateKeyByte(char s[]) {
-    static int i=0
-    static int j=0
-    i = (i+1) % 256
-    j = (j+S[i]) % 256
-
-    // char temp;
-    // temp = S[i];
-    // S[i] = S[j];
-    // S[j] = temp;
+    static int i=0;
+    static int j=0;
+    char keyByte;
+    i = (i+1) % 256;
+    j = (j+S[i]) % 256;
 
     swap(&S[i],&S[j]);
 
-    t = (S[i]+S[j]) % 256
-    return keyByte = S[t]
+    t = (S[i]+S[j]) % 256;
+    return keyByte = S[t];
 }
 
 void swap(int *a, int *b) {
