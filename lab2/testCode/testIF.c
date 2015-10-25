eacc6dfffb4943fc2639054f2585018bfd11aa16
Oscar <oscarwinglee@gmail.com>
2015-10-25 19:00:58 -0400

    [NOT TESTED] Made parsefile and parses buffer into tokens

---
+++ lab2/testCode/testIF.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <assert.h>
+#include <assert.h>
+#include <stdlib.h>
+
+struct Instr {
+  int regDst;
+  int aluSrc;
+  int aluOp;
+  int memWrite;
+  int memRead;
+  int memToReg;
+  int regWrite;
+
+  char opcode[];
+  char args[3];
+  int rs, rt, rd;
+};
+
+struct Instr instr_fields[n];
+
+int parseFile(char* in) {
+  FILE* inputF = fopen(in, "r");
+
+  char delimiters[]=", " // TODO: Maybe add the $ in here to be delimited
+  char buffer[100];
+  int lineNum= 0;
+  while (fgets(buffer, 100, inputF)) {
+    instr_fields[lineNum].opcode[] = strtok[buffer, delimiters]
+    instr_fields[lineNum].args[0] = strtok[NULL, delimiters]
+    instr_fields[lineNum].args[1] = strtok[NULL, delimiters]
+    instr_fields[lineNum].args[2] = strtok[NULL, delimiters]
+
+    lineNum++;
+  }
+  fclose(inputF);
+  return 0;
+
+}
+struct Instr IF(char* in) {
+ // int i;
+ // for(i=0;i<EOF;i++) {
+ //   if(strcmp("HALTINSTRUCTION", fgets(inputF)) == 0) {
+ //     exit(0);
+ //   }
+ // }
+
+    return newInstr;
+}
+
+int main (int argc, char *argv[]) {
+
+
+}
