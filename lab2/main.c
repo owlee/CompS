c477ab6a34f4307761f69c66d74778dda513ab01
Oscar <oscarwinglee@gmail.com>
2015-10-25 16:12:39 -0400

    Tested Mips Reg initialization and added to the main

--- lab2/main.c
+++ lab2/main.c
@@ -1,33 +1,50 @@
 // Author: Shikang Xu; ECE 353 TA
 
 // List the full names of ALL group members at the top of your code.
+// Oscar Lee
+//
+//
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <math.h>
 #include <assert.h>
-//feel free to add here any additional library names you may need 
+//feel free to add here any additional library names you may need
+
 #define SINGLE 1
 #define BATCH 0
 #define REG_NUM 32
+
+//Global Variables
+long mips_reg[REG_NUM];
+  //For REG_NUM == 32
+  long *$zero = &mips_reg[0], *$0 = &mips_reg[0], *$al = &mips_reg[1] , *$1  = &mips_reg[1] , *$v0 = &mips_reg[2] , *$2  = &mips_reg[2] , *$v1 = &mips_reg[3] , *$3  = &mips_reg[3] , *$a0 = &mips_reg[4] , *$4  = &mips_reg[4] , *$a1 = &mips_reg[5] , *$5  = &mips_reg[5] , *$a2 = &mips_reg[6] , *$6  = &mips_reg[6] , *$a3 = &mips_reg[7] , *$7  = &mips_reg[7] , *$t0 = &mips_reg[8] , *$8  = &mips_reg[8] , *$t1 = &mips_reg[9] , *$9  = &mips_reg[9] , *$t2 = &mips_reg[10] , *$10 = &mips_reg[10] , *$t3 = &mips_reg[11] , *$11 = &mips_reg[11] , *$t4 = &mips_reg[12] , *$12 = &mips_reg[12] , *$t5 = &mips_reg[13] , *$13 = &mips_reg[13] , *$t6 = &mips_reg[14] , *$14 = &mips_reg[14] , *$t7 = &mips_reg[15] , *$15 = &mips_reg[15] , *$s0 = &mips_reg[16] , *$16 = &mips_reg[16] , *$s1 = &mips_reg[17] , *$17 = &mips_reg[17] , *$s2 = &mips_reg[18] , *$18 = &mips_reg[18] , *$s3 = &mips_reg[19] , *$19 = &mips_reg[19] , *$s4 = &mips_reg[20] , *$20 = &mips_reg[20] , *$s5 = &mips_reg[21] , *$21 = &mips_reg[21] , *$s6 = &mips_reg[22] , *$22 = &mips_reg[22] , *$s7 = &mips_reg[23] , *$23 = &mips_reg[23] , *$t8 = &mips_reg[24] , *$24 = &mips_reg[24] , *$t9 = &mips_reg[25] , *$25 = &mips_reg[25] , *$k0 = &mips_reg[26] , *$26 = &mips_reg[26] , *$k1 = &mips_reg[27] , *$27 = &mips_reg[27] , *$gp = &mips_reg[28] , *$28 = &mips_reg[28] , *$sp = &mips_reg[29] , *$29 = &mips_reg[29] , *$fp = &mips_reg[30] , *$30 = &mips_reg[30] , *$ra = &mips_reg[31] , *$31 = &mips_reg[31];
+
+//Function Content
+
+
+
 main (int argc, char *argv[]){
 	int sim_mode=0;//mode flag, 1 for single-cycle, 0 for batch
-	int c,m,n;
-	int i;//for loop counter
-	long mips_reg[REG_NUM];
 	long pgm_c=0;//program counter
 	long sim_cycle=0;//simulation cycle counter
+
 	//define your own counter for the usage of each pipeline stage here
-	
+
 	int test_counter=0;
-	FILE *input=NULL;
-	FILE *output=NULL;
+	FILE *input=NULL;     // Input a set of instructions
+	FILE *output=NULL;    // Output the data
+
+  int i;
 	printf("The arguments are:");
-	
 	for(i=1;i<argc;i++){
 		printf("%s ",argv[i]);
 	}
-	printf("\n");
+
+  //choosing the simulation mode
+	int c,m,n;
+  printf("\n");
 	if(argc==7){
 		if(strcmp("-s",argv[1])==0){
 			sim_mode=SINGLE;
@@ -39,34 +56,36 @@ main (int argc, char *argv[]){
 			printf("Wrong sim mode chosen\n");
 			exit(0);
 		}
-		
-		m=atoi(argv[2]);
-		n=atoi(argv[3]);
-		c=atoi(argv[4]);
+
+		m=atoi(argv[2]); // number of cycles needed by miltiplication
+		n=atoi(argv[3]); // other operation
+		c=atoi(argv[4]); // memory access
 		input=fopen(argv[5],"r");
 		output=fopen(argv[6],"w");
-		
 	}
-	
+
 	else{
 		printf("Usage: ./sim-mips -s m n c input_name output_name (single-sysle mode)\n or \n ./sim-mips -b m n c input_name  output_name(batch mode)\n");
 		printf("m,n,c stand for number of cycles needed by multiplication, other operation, and memory access, respectively\n");
 		exit(0);
 	}
+
 	if(input==NULL){
 		printf("Unable to open input or output file\n");
 		exit(0);
 	}
+
 	if(output==NULL){
 		printf("Cannot create output file\n");
 		exit(0);
 	}
+
 	//initialize registers and program counter
 	if(sim_mode==1){
 		for (i=0;i<REG_NUM;i++){
 			mips_reg[i]=0;
 		}
 	}
-	
+
 	//start your code from here
 }
