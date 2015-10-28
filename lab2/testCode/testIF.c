de1fe7c1a54d705efe4a5c5e3372bad66faf8863
Oscar <oscarwinglee@gmail.com>
2015-10-28 12:19:45 -0400

    [TESTED] Parsing files now work as expected

--- lab2/testCode/testIF.c
+++ lab2/testCode/testIF.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <assert.h>
-#include <assert.h>
 #include <stdlib.h>
+#include <string.h>
 
 struct Instr {
   int regDst;
@@ -12,45 +12,56 @@ struct Instr {
   int memToReg;
   int regWrite;
 
-  char opcode[];
-  char args[3];
+  char opcode[10], arg0[10], arg1[10], arg2[10];
   long rs, rt, rd;
 };
 
-struct Instr instr_fields[1];
+struct Instr instr_fields[2];
 
 int parseFile(char* in) {
-  
-  FILE* inputF = fopen(in, "r");
-  char delimiters[]=", " // TODO: Maybe add the $ in here to be delimited
-  char buffer[100];
+
+  FILE* fp = fopen(in, "r");
+  assert(fp!=NULL);
+  char delimiters[]=", ;$\n"; // TODO: Maybe add the $ in here to be delimited
+  char buffer[1024];
   int lineNum=0;
-  char *token;
-  
-  while (fgets(buffer, 100, inputF)) {
-    instr_fields[lineNum].opcode = strtok(buffer,delimiters);
-    instr_fields[lineNum].args[0] = strtok(NULL,delimiters);
-    instr_fields[lineNum].args[1] = strtok(NULL,delimiters);
-    instr_fields[lineNum].args[2] = strtok(NULL,delimiters);
-    
-    assert(strtok(NULL,delimiters) == NULL); // Checks for additional parameters
+
+  while (fgets(buffer, sizeof(buffer), fp) != NULL) {
+    strcpy(instr_fields[lineNum].opcode, strtok(buffer,delimiters));
+    strcpy(instr_fields[lineNum].arg0, strtok(NULL,delimiters));
+    strcpy(instr_fields[lineNum].arg1, strtok(NULL,delimiters));
+    strcpy(instr_fields[lineNum].arg2, strtok(NULL,delimiters));
 
     lineNum++;
   }
-  fclose(inputF);
+  fclose(fp);
   return 0;
 }
-struct Instr IF(char* in) {
- // int i;
- // for(i=0;i<EOF;i++) {
- //   if(strcmp("HALTINSTRUCTION", fgets(inputF)) == 0) {
- //     exit(0);
- //   }
- // }
 
-    return newInstr;
-}
+struct Instr IF(char* in) {
+  // int i;
+  // for(i=0;i<EOF;i++) {
+  //   if(strcmp("HALTINSTRUCTION", fgets(inputF)) == 0) {
+  //     exit(0);
+  //   }
+  // }
+  struct Instr nop;
+  return nop;
+  //return newInstr;
+};
 
 int main (int argc, char *argv[]) {
-    assert(parseFile("./textInstructions.txt")==0);
+  assert(parseFile("./textInstructions.txt")==0);
+
+  printf("String : %s\n", instr_fields[0].opcode);
+  printf("String : %s\n", instr_fields[0].arg0);
+  printf("String : %s\n", instr_fields[0].arg1);
+  printf("String : %s\n", instr_fields[0].arg2);
+
+  printf("String : %s\n", instr_fields[1].opcode);
+  printf("String : %s\n", instr_fields[1].arg0);
+  printf("String : %s\n", instr_fields[1].arg1);
+  printf("String : %s\n", instr_fields[1].arg2);
+
+
 }
