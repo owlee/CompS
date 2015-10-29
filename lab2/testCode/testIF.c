97b4685245757d2f1386005b7534014ea0334f18
Oscar <oscarwinglee@gmail.com>
2015-10-28 23:53:14 -0400

    change to opcode to func

--- lab2/testCode/testIF.c
+++ lab2/testCode/testIF.c
@@ -13,7 +13,7 @@ struct Instr {
   int memToReg;
   int regWrite;
   
-  char opcode[10], arg0[10], arg1[10], arg2[10];
+  char func[10], arg0[10], arg1[10], arg2[10];
   long rs, rt, rd;
 };
 
@@ -36,7 +36,7 @@ int parseFile(char* in) {
       i++;
     }
     
-    strcpy(instr_fields[lineNum].opcode, strtok(buffer,delimiters));
+    strcpy(instr_fields[lineNum].func, strtok(buffer,delimiters));
     strcpy(instr_fields[lineNum].arg0, strtok(NULL,delimiters));
     strcpy(instr_fields[lineNum].arg1, strtok(NULL,delimiters));
     strcpy(instr_fields[lineNum].arg2, strtok(NULL,delimiters));
@@ -62,17 +62,17 @@ struct Instr IF(char* in) {
 int main (int argc, char *argv[]) {
   assert(parseFile("./textInstructions.txt")==0);
   
-  printf("String : %s\n", instr_fields[0].opcode);
+  printf("String : %s\n", instr_fields[0].func);
   printf("String : %s\n", instr_fields[0].arg0);
   printf("String : %s\n", instr_fields[0].arg1);
   printf("String : %s\n", instr_fields[0].arg2);
   
-  printf("String : %s\n", instr_fields[1].opcode);
+  printf("String : %s\n", instr_fields[1].func);
   printf("String : %s\n", instr_fields[1].arg0);
   printf("String : %s\n", instr_fields[1].arg1);
   printf("String : %s\n", instr_fields[1].arg2);
   
-  printf("String : %s\n", instr_fields[2].opcode);
+  printf("String : %s\n", instr_fields[2].func);
   printf("String : %s\n", instr_fields[2].arg0);
   printf("String : %s\n", instr_fields[2].arg1);
   printf("String : %s\n", instr_fields[2].arg2);
