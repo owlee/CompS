8f76e657e6191c8872b60222a130e92a746a8a06
Oscar <oscarwinglee@gmail.com>
2015-10-29 01:12:21 -0400

    Added a line count for input file  and reorganized structures.

--- lab2/testCode/testIF.c
+++ lab2/testCode/testIF.c
@@ -17,21 +17,19 @@ struct Instr {
   long rs, rt, rd;
 };
 
-struct Instr instr_fields[10];
-
-int parseFile(char* in) {
+int parseFile(char* in, struct Instr instr_fields[]) {
   
   FILE* fp = fopen(in, "r");
   assert(fp!=NULL);
-  char delimiters[]=", ;$\n"; // TODO: Maybe add the $ in here to be delimited
-  char buffer[40];
-  int lineNum=0;
   
+  char delimiters[]=", ;$\n"; // TODO: Maybe add the $ in here to be delimited
+  int lineNum=0;
+  int i;
+  char buffer[40];
   while (fgets(buffer, sizeof(buffer), fp) != NULL) {
-    int i=0;
+    i=0;
     
     while (buffer[i]) {
-      
       buffer[i] =   tolower(buffer[i]);
       i++;
     }
@@ -47,33 +45,56 @@ int parseFile(char* in) {
   return 0;
 }
 
-struct Instr IF(char* in) {
+int countLine(char *in) {
+  FILE *fp = fopen(in, "r");
+  int lines = 0;
+  char ch;
+  
+  ch = getc(fp);
+  while (ch != EOF) {
+    if (ch == '\n') {
+      lines = lines+1;
+    }
+    ch = getc(fp);
+  }
+  return lines+1;
+}
+
+struct Instr IF(char* in, struct Instr instr_fields[]) {
   // int i;
   // for(i=0;i<EOF;i++) {
   //   if(strcmp("HALTINSTRUCTION", fgets(inputF)) == 0) {
   //     exit(0);
   //   }
   // }
+  assert(parseFile(in, instr_fields)==0);
+  
   struct Instr nop;
   return nop;
+  
   //return newInstr;
 };
 
 int main (int argc, char *argv[]) {
-  assert(parseFile("./textInstructions.txt")==0);
+  char *textInst = (char*) malloc(30);
+  strcpy(textInst, "./textInstructions.txt");
   
-  printf("String : %s\n", instr_fields[0].func);
-  printf("String : %s\n", instr_fields[0].arg0);
-  printf("String : %s\n", instr_fields[0].arg1);
-  printf("String : %s\n", instr_fields[0].arg2);
+  struct Instr instr_mem[countLine(textInst)];
   
-  printf("String : %s\n", instr_fields[1].func);
-  printf("String : %s\n", instr_fields[1].arg0);
-  printf("String : %s\n", instr_fields[1].arg1);
-  printf("String : %s\n", instr_fields[1].arg2);
+  printf("Number of lines in file: %i\n", countLine(textInst));
   
-  printf("String : %s\n", instr_fields[2].func);
-  printf("String : %s\n", instr_fields[2].arg0);
-  printf("String : %s\n", instr_fields[2].arg1);
-  printf("String : %s\n", instr_fields[2].arg2);
+  //  printf("String : %s\n", instr_fields[0].func);
+  //  printf("String : %s\n", instr_fields[0].arg0);
+  //  printf("String : %s\n", instr_fields[0].arg1);
+  //  printf("String : %s\n", instr_fields[0].arg2);
+  //
+  //  printf("String : %s\n", instr_fields[1].func);
+  //  printf("String : %s\n", instr_fields[1].arg0);
+  //  printf("String : %s\n", instr_fields[1].arg1);
+  //  printf("String : %s\n", instr_fields[1].arg2);
+  //
+  //  printf("String : %s\n", instr_fields[2].func);
+  //  printf("String : %s\n", instr_fields[2].arg0);
+  //  printf("String : %s\n", instr_fields[2].arg1);
+  //  printf("String : %s\n", instr_fields[2].arg2);
 }
