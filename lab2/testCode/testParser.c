a2338c065681bfd993d51d914770b61cbae3a3e8
Oscar <oscarwinglee@gmail.com>
2015-11-01 14:10:40 -0500

    [NOT TESTED] parsing a string and making object

--- lab2/testCode/testParser.c
+++ lab2/testCode/testParser.c
@@ -24,8 +24,7 @@ struct Instr {
 struct Instr instr_mem[10];
 
 struct Instr progScanner(char *);
-
-int getEnum(char* str);
+int regConverter(char*);
 
 int main (int argc, char *argv[]) {
   char *textInst = (char*) malloc(30);
@@ -92,19 +91,75 @@ struct Instr progScanner(char* buffer) {
     exit(0);
   }
   
+    // parser
+  // opcode
   struct Instr instrObj;
   strcpy(instrObj.opcode, token);
   
+  // arg0
+  token = strtok(sanitize, delimiters2);
+  instrObj.arg0 = regConverter(token);
   
-  //if (strcmp(token, ''))
+  if (strcmp(instrObj.opcode, "sw") == 0) {
+    // arg1
+    token = strtok(sanitize, delimiters2);
+    instrObj.arg1 = atoi(token);
+    
+  } else if (strcmp(instrObj.opcode, "lw") == 0) {
+    // arg1
+    token = strtok(sanitize, delimiters2);
+    instrObj.arg1 = atoi(token);
+    
+  } else {
+    // arg1
+    token = strtok(sanitize, delimiters2);
+    instrObj.arg1 = regConverter(token);
+  }
   
+  // arg2
+  token = strtok(sanitize, delimiters2);
+  instrObj.arg2 = regConverter(token);
   
-  
-  
-  
-  // parser()
-  
-  
-  struct Instr nop;
-  return nop;
+  return instrObj;
+}
+
+int regConverter(char* str) {
+  int ret = 0;
+  if (strcmp(str, "zero") == 0) { ret = 0; }
+  else if (strcmp(str, "at") == 0) { ret = 1; }
+  else if (strcmp(str, "v0") == 0) { ret = 2; }
+  else if (strcmp(str, "v1") == 0) { ret = 3; }
+  else if (strcmp(str, "a0") == 0) { ret = 4; }
+  else if (strcmp(str, "a1") == 0) { ret = 5; }
+  else if (strcmp(str, "a2") == 0) { ret = 6; }
+  else if (strcmp(str, "a3") == 0) { ret = 7; }
+  else if (strcmp(str, "t0") == 0) { ret = 8; }
+  else if (strcmp(str, "t1") == 0) { ret = 9; }
+  else if (strcmp(str, "t2") == 0) { ret = 10; }
+  else if (strcmp(str, "t3") == 0) { ret = 11; }
+  else if (strcmp(str, "t4") == 0) { ret = 12; }
+  else if (strcmp(str, "t5") == 0) { ret = 13; }
+  else if (strcmp(str, "t6") == 0) { ret = 14; }
+  else if (strcmp(str, "t7") == 0) { ret = 15; }
+  else if (strcmp(str, "s0") == 0) { ret = 16; }
+  else if (strcmp(str, "s1") == 0) { ret = 17; }
+  else if (strcmp(str, "s2") == 0) { ret = 18; }
+  else if (strcmp(str, "s3") == 0) { ret = 19; }
+  else if (strcmp(str, "s4") == 0) { ret = 20; }
+  else if (strcmp(str, "s5") == 0) { ret = 21; }
+  else if (strcmp(str, "s6") == 0) { ret = 22; }
+  else if (strcmp(str, "s7") == 0) { ret = 23; }
+  else if (strcmp(str, "t8") == 0) { ret = 24; }
+  else if (strcmp(str, "t9") == 0) { ret = 25; }
+  else if (strcmp(str, "k0") == 0) { ret = 26; }
+  else if (strcmp(str, "k1") == 0) { ret = 27; }
+  else if (strcmp(str, "gp") == 0) { ret = 28; }
+  else if (strcmp(str, "sp") == 0) { ret = 29; }
+  else if (strcmp(str, "fp") == 0) { ret = 30; }
+  else if (strcmp(str, "ra") == 0) { ret = 31; }
+  else {
+    printf("Register name error: %s", str);
+    exit(0);
+  }
+  return ret;
 }
