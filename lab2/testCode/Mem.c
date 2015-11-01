77a1018e261939b42df5cb0e98207263c6bdee4e
Calvin <cptran0@gmail.com>
2015-11-01 18:24:29 -0500

    merge

--- lab2/testCode/Mem.c
+++ lab2/testCode/Mem.c
@@ -1,12 +1,12 @@
 void MemStage(){ //INPUT LATCH: EXMEM ; OUTPUT LATCH = MEMWB
-	struct Instr temp = EXMEM.inst;
+	struct Instr temp = EXMEM.data;
 	if(EXMEM.validBit == 1) //valid inst test
 		EXMEM.validBit = 0; //has been read and invalidated.
 		assert(!(temp.memWrite && temp.memRead));
 		if(temp.memWrite == 1)  //sw
-			mem_reg[temp.product] = temp.rt;
-		if(temp.memRead == 1)   //lw
+			mem_reg[temp.product] = mips_reg[temp.rt];
+		else if(temp.memRead == 1)   //lw
 			temp.product = mem_reg[temp.product]; //memory value to be used in WB.
-		MEMWB.inst.product = temp.product; //push instr into output latch
+		MEMWB.data.product = temp.product; //push instr into output latch
 		MEMWB.validBit = 1; //validates output latch
 	}
