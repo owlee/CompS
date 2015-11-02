29f3f01d81f412fa18db0f1b7e2730c2201557ba
Oscar <oscarwinglee@gmail.com>
2015-11-02 15:06:58 -0500

    minor clean up

--- lab2/testCode/ID.c
+++ lab2/testCode/ID.c
@@ -1,82 +1,79 @@
 void ID(){
-	
-	IFID.data = &instr_mem[PC]; 
-	struct Instr test;
-if(IFID.validBit == 1){
-	test = *(IFID.data);	
-	if(test.imm > 65535){
-			printf("%s", "Immediate field is out of range");
+
+	if(IFID.validBit == 1){
+		struct Instr test = *(IFID.data);	
+		if(test.imm > 65535){
+				printf("%s", "Immediate field is out of range");
+				exit(0);
+			}
+			
+			if(strcmp(test.opcode, "add") == 0){	
+			test.rd = test.arg0;
+			test.rs = test.arg1;
+			test.rt = test.arg2;
+			test.memRead = 0;
+			test.memWrite = 0;
+			test.regWrite = 1;
+			}
+		else if(strcmp(test.opcode, "addi") == 0){			
+		    test.rd = test.arg0;
+			test.rs = test.arg1;
+			test.imm = test.arg2;
+			test.memRead = 0;
+			test.memWrite = 0;
+			test.regWrite = 1;
+		}
+		else if(strcmp(test.opcode, "sub") == 0){
+			test.rd = test.arg0;
+			test.rs = test.arg1;
+			test.rt = test.arg2;
+			test.memRead = 0;
+			test.memWrite = 0;
+			test.regWrite = 1;
+		}
+			
+		else if(strcmp(test.opcode, "mul") == 0){
+			test.rd = test.arg0;
+			test.rs = test.arg1;
+			test.rt = test.arg2;
+			test.memRead = 0;
+			test.memWrite = 0;
+			test.regWrite = 1;
+		}
+			
+		else if(strcmp(test.opcode, "beq") == 0){
+			test.rs = test.arg0;
+			test.rt = test.arg1;
+			test.imm = test.arg2;
+			test.memRead = 0;
+			test.memWrite = 0;
+			test.regWrite = 0;
+		}
+		
+		else if(strcmp(test.opcode, "lw") == 0){     
+			test.rd = test.arg0;
+			test.imm = test.arg1;
+			test.rs = test.arg2;
+			test.memWrite = 0;
+			test.memRead = 1;
+			test.regWrite = 1;
+		}
+			
+		else if(strcmp(test.opcode, "sw") == 0){
+			test.rt = test.arg0;
+			test.imm = test.arg1;
+			test.rs = test.arg2;
+			test.memRead = 0;
+			test.regWrite = 0;
+			test.memWrite = 1;
+		}
+		else{
+			printf("%s", "Invalid function");
 			exit(0);
 		}
-		
-		if(strcmp(test.opcode, "add") == 0){	
-		test.rd = test.arg0;
-		test.rs = test.arg1;
-		test.rt = test.arg2;
-		test.memRead = 0;
-		test.memWrite = 0;
-		test.regWrite = 1;
-		}
-	else if(strcmp(test.opcode, "addi") == 0){			
-	    test.rd = test.arg0;
-		test.rs = test.arg1;
-		test.imm = test.arg2;
-		test.memRead = 0;
-		test.memWrite = 0;
-		test.regWrite = 1;
-	}
-	else if(strcmp(test.opcode, "sub") == 0){
-		test.rd = test.arg0;
-		test.rs = test.arg1;
-		test.rt = test.arg2;
-		test.memRead = 0;
-		test.memWrite = 0;
-		test.regWrite = 1;
-	}
-		
-	else if(strcmp(test.opcode, "mult") == 0){
-		test.rd = test.arg0;
-		test.rs = test.arg1;
-		test.rt = test.arg2;
-		test.memRead = 0;
-		test.memWrite = 0;
-		test.regWrite = 1;
-	}
-		
-	else if(strcmp(test.opcode, "beq") == 0){
-		test.rs = test.arg0;
-		test.rt = test.arg1;
-		test.imm = test.arg2;
-		test.memRead = 0;
-		test.memWrite = 0;
-		test.regWrite = 0;
-	}
-	
-	else if(strcmp(test.opcode, "lw") == 0){     
-		test.rd = test.arg0;
-		test.imm = test.arg1;
-		test.rs = test.arg2;
-		test.memWrite = 0;
-		test.memRead = 1;
-		test.regWrite = 1;
-	}
-		
-	else if(strcmp(test.opcode, "sw") == 0){
-		test.rt = test.arg0;
-		test.imm = test.arg1;
-		test.rs = test.arg2;
-		test.memRead = 0;
-		test.regWrite = 0;
-		test.memWrite = 1;
-	}
-	else{
-		printf("%s", "Invalid function");
-		exit(0);
-	}
 
 	IFID.validBit = 0;
 	IDEX.validBit = 1;
-	IDEX.data = &instr_mem[PC];
+	IDEX.data = &test;
 	}
 }
-
--- lab2/testCode/Mem.c
+++ lab2/testCode/Mem.c
@@ -1,12 +1,19 @@
-void MemStage(){ //INPUT LATCH: EXMEM ; OUTPUT LATCH = MEMWB
-	struct Instr temp = EXMEM.data;
-	if(EXMEM.validBit == 1) //valid inst test
+void MEM(){ //INPUT LATCH: EXMEM ; OUTPUT LATCH = MEMWB
+
+	if(EXMEM.validBit == 1) { //valid inst test
+		struct Instr temp = *EXMEM.data;
+
 		EXMEM.validBit = 0; //has been read and invalidated.
 		assert(!(temp.memWrite && temp.memRead));
-		if(temp.memWrite == 1)  //sw
+
+		if(temp.memWrite == 1) {  //sw
 			mem_reg[temp.product] = mips_reg[temp.rt];
-		else if(temp.memRead == 1)   //lw
+
+		} else if(temp.memRead == 1) { //lw
 			temp.product = mem_reg[temp.product]; //memory value to be used in WB.
-		MEMWB.data.product = temp.product; //push instr into output latch
+		}
+
+		MEMWB.data = &temp; //push instr into output latch
 		MEMWB.validBit = 1; //validates output latch
 	}
+}
